398968e9679f7751fe3a99cdfd2d5377317a227a
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2025-03-31 13:48:03 +0300

    feat: create project structure with model folder and add configuration

---
+++ src/main/java/com/shop/ecommecre/EcommecreApplication.java
@@ -0,0 +1,13 @@
+package com.shop.ecommecre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommecreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommecreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/ecommecre/EcommecreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.ecommecre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommecreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
