ce6e162312fc974ae08399bd7f4641ceeb05fe25
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2025-04-06 22:23:36 +0300

    feat: add Cart and CartItem models

---
+++ src/main/java/com/shop/ecommecre/model/CartItem.java
@@ -0,0 +1,41 @@
+package com.shop.ecommecre.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal pricePerItem;
+    private BigDecimal totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    public void setTotalPrice() {
+        this.totalPrice = this.pricePerItem.multiply(new BigDecimal(quantity));
+    }
+}
