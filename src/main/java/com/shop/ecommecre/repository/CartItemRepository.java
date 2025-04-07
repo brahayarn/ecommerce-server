09535a4b5b5b4b9077e0e05eaee846f034c75a10
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2025-04-07 18:34:15 +0300

    fix: issue with deleteAll method in CartItemRepository

--- src/main/java/com/shop/ecommecre/repository/CartItemRepository.java
+++ src/main/java/com/shop/ecommecre/repository/CartItemRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.shop.ecommecre.model.CartItem;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    void deleteAll(Long id);
+    void deleteByCartId(Long id);
 }
     
